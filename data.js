26ede68dcef489147b996330ccad71430401a2ad
Israel Teixeira <israelst@gmail.com>
2014-10-27 23:59:29 -0200

    Download data if it has not been modified

--- data.js
+++ data.js
@@ -1,23 +1,32 @@
 var fs = require('fs'),
     https = require('https'),
     path = require('path'),
-    zlib = require('zlib');
+    zlib = require('zlib'),
+    csvFile = path.join('static', 'data', 'segpub.csv'),
+    gzFile = csvFile + '.gz',
+    headers = {'Accept-Encoding': 'gzip'};
 
-function dataFileStream(filename){
-    var filePath = path.join('static', 'data', filename);
-    return fs.createWriteStream(filePath);
+if(fs.existsSync(gzFile)){
+    headers['If-Modified-Since'] = fs.statSync(gzFile).mtime.toGMTString();
 }
 
+console.log('Gathering data...');
 // We can't trust in response file type readers
 // and the certificate is self signed
 https.get({
     host: 'static.hpc.pypln.org',
     path: '/segpub.csv.gz',
     rejectUnauthorized: false,
-    headers: {'accept-encoding': 'gzip'}
+    headers: headers
 }).on('response', function(response){
-    response.pipe(dataFileStream('segpub.csv.gz'));
+    console.info('Status code: ', response.statusCode);
+    response.pipe(fs.createWriteStream(gzFile));
     response.pipe(zlib.createGunzip())
-            .pipe(dataFileStream('segpub.csv'));
+            .pipe(fs.createWriteStream(csvFile));
+    response.on('end', function(){
+        var mtime = new Date(this.headers['last-modified']);
+        fs.utimesSync(gzFile, mtime, mtime);
+        console.log('Done.');
+    });
 });
 
