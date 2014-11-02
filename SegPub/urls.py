e3902f442a35a854d39c41646b3bdaa7dafa8c0b
Israel Teixeira <israelst@gmail.com>
2014-11-01 23:16:21 -0200

    Remove admin urls

--- SegPub/urls.py
+++ SegPub/urls.py
@@ -6,5 +6,4 @@ admin.autodiscover()
 
 urlpatterns = patterns('SegPub.polmil.views',
     url(r'^$', TemplateView.as_view(template_name="polmil/index.html")),
-    url(r'^admin/', include(admin.site.urls)),
     )
