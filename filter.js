d166ff22656f216615b017a15d35c4bf92ebb3fc
Israel Teixeira <israelst@gmail.com>
2014-11-09 15:48:08 -0200

    Add data selector

--- filter.js
+++ filter.js
@@ -6,17 +6,29 @@ var fs = require('fs'),
     stringfier = csv.stringify(),
     file = fs.createReadStream(csvFile);
 
+function selector(){
+    var keys = [].slice.call(arguments);
+    return csv.transform(function(record){
+        var selection = [];
+        keys.forEach(function(key){
+            selection.push(record[key]);
+        });
+        return selection;
+    });
+}
+
 function filterByKind(kind){
     return csv.transform(function(record){
         var initialKind = record[6],
             finalKind = record[8];
         if(initialKind == kind || finalKind == kind){
-            return [initialKind, finalKind];
+            return record;
         }
     });
 }
 
 file.pipe(parser)
     .pipe(filterByKind('Roubo'))
+    .pipe(selector(6, 8))
     .pipe(stringfier)
     .pipe(process.stdout);
