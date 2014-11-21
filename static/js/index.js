c0073df0a8357cd350aa6e4c81f9486c7c38bd18
Israel Teixeira <israelst@gmail.com>
2014-11-21 19:49:24 -0200

    Add summary filter

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,6 @@
 var L = require('leaflet'),
     $ = require('jquery'),
+    d3 = require('d3'),
     mapa = L.map('mapaleaf', {
         attributionControl: false,
         zoomControl: false
@@ -51,5 +52,42 @@ $.ajax ({
     complete: function() {
         $('#carregando').delay(500).fadeOut('slow');
     }
+});
+
+d3.json('/incidents/summary', function(json){
+    var data = d3.entries(json).sort(function(a, b){
+        return b.value - a.value;
+    }),
+    max = d3.max(data, function(d){ return d.value;}),
+    summary = d3.select('div.boxes')
+                .append('div')
+                .attr('id', 'summary')
+                .append('div')
+                .selectAll('div')
+                .data(data)
+                .enter()
+                .append('div');
+
+    function slug(d){
+        return d.key.split(' ').join('');
+    }
+
+    summary.append('input')
+        .attr('id', slug)
+        .attr('type', 'radio')
+        .attr('name', 'finalKind')
+        .on('change', function(value){
+            console.log('changed to', value.key);
+        });
+
+    summary.append('label')
+        .attr('class', slug)
+        .attr('for', slug)
+        .append('span')
+        .attr('class', 'bar')
+        .style('width', function(d){
+            return ((d.value / max) * 100) + '%';
+        })
+        .text(function(d){ return d.key;});
 });
 
