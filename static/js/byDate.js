842615f06f544f8d74beeb68733f943cace29090
Israel Teixeira <israelst@gmail.com>
2015-01-25 18:19:52 -0200

    Show the distribution of incidentes on time

--- static/js/byDate.js
+++ static/js/byDate.js
@@ -1,12 +1,29 @@
-var d3 = require('d3');
+var d3 = require('d3'), 
+    timeParser = d3.time.format('%d/%m/%Y');
+
+function value(d){ return d.value; }
+function date(d){ return timeParser.parse(d.key); }
 
 exports.byDate = function(){
     return function(container){
-        var data = d3.entries(container.datum());
-        container
+        var data = d3.entries(container.datum()),
+            height = 150,
+            x = d3.time.scale().domain(d3.extent(data, date)).range([0, 368]),
+            y = d3.scale.linear().domain([0, d3.max(data.map(value))]).range([height, 0]);
+        container.append('svg')
             .selectAll('rect')
             .data(data)
             .enter()
-            .append('rect');
+            .append('rect')
+            .attr('x', function(d){
+                return x(date(d));
+            })
+            .attr('y', function(d){
+                return y(value(d));
+            })
+            .attr('width', 10)
+            .attr('height', function(d){
+                return height - y(value(d));
+            });
     };
 };
