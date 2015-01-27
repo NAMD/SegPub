5bd091a2dd0b5e84dd9e556b0e5a41bc30967406
Israel Teixeira <israelst@gmail.com>
2015-01-26 23:57:26 -0200

    Save svg reference

--- static/js/byDate.js
+++ static/js/byDate.js
@@ -6,12 +6,13 @@ function date(d){ return timeParser.parse(d.key); }
 
 exports.byDate = function(){
     return function(container){
-        var data = d3.entries(container.datum()),
+        var svg = container.append('svg'),
+            data = d3.entries(container.datum()),
             height = 150,
             x = d3.time.scale().domain(d3.extent(data, date)).range([0, 368]),
             y = d3.scale.linear().domain([0, d3.max(data.map(value))]).range([height, 0]);
-        container.append('svg')
-            .selectAll('rect')
+
+        svg.selectAll('rect')
             .data(data)
             .enter()
             .append('rect')
