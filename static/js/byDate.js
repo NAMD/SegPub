298d1ed061a9195099b95c1700ce2a73204fca36
Israel Teixeira <israelst@gmail.com>
2015-01-28 22:56:48 -0200

    Fix date range. Including the last date

--- static/js/byDate.js
+++ static/js/byDate.js
@@ -3,13 +3,17 @@ var d3 = require('d3'),
 
 function value(d){ return d.value; }
 function date(d){ return timeParser.parse(d.key); }
+function plusOneDay(date){
+    var oneDay = 86400000;
+    return new Date(date.getTime() + oneDay);
+}
 
 exports.byDate = function(){
     return function(container){
         var svg = container.append('svg'),
             data = d3.entries(container.datum()),
             height = 150,
-            daysDomain = d3.time.days.apply(this, d3.extent(data, date)),
+            daysInterval = d3.time.days(d3.min(data, date), plusOneDay(d3.max(data, date))),
             x = d3.scale.ordinal().domain(daysInterval).rangeBands([0, 386], 0.25, 2),
             y = d3.scale.linear().domain([0, d3.max(data.map(value))]).range([height, 0]),
             xAxis = d3.svg.axis().scale(x).orient("bottom").tickFormat(function(d){
