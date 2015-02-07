482a4e17de198d595f3bc73eeed97067dc7efdd7
Israel Teixeira <israelst@gmail.com>
2015-02-06 23:56:48 -0200

    Create month interval

--- static/js/byDate.js
+++ static/js/byDate.js
@@ -6,6 +6,9 @@ function date(d){ return timeParser.parse(d.key); }
 function plusOneDay(date){
     return new Date(date).setDate(date.getDate() + 1);
 }
+function plusOneMonth(date){
+    return new Date(date).setMonth(date.getMonth() + 1);
+}
 
 exports.byDate = function(){
     return function(container){
@@ -14,6 +17,7 @@ exports.byDate = function(){
             height = 100,
             marginTop = 20,
             daysInterval = d3.time.days(d3.min(data, date), plusOneDay(d3.max(data, date))),
+            monthsInterval = d3.time.months(d3.min(data, date), plusOneMonth(d3.max(data, date))),
             x = d3.scale.ordinal().domain(daysInterval).rangeBands([0, width], 0.25, 2),
             y = d3.scale.linear().domain([0, d3.max(data.map(value))]).range([height - marginTop, 0]),
             xAxis = d3.svg.axis().scale(x).tickFormat(function(d){
